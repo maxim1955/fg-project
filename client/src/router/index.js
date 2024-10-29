07342313088c61d215bbc6f534d1c6e12f936a33
Tatya <Tatya-Gabrielyan@mail.ru>
2024-10-29 21:54:08 +0300

    добавила остановку таймера

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter,createWebHistory} from 'vue-router'
 import routes from './routes'
 import {useTimerAndDateStore} from "../store/TimerStore.js";
 import userStore from "../store/UserStore.js";
+import {useTimerStore} from "../store/TimeStore.js";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -17,7 +18,9 @@ router.beforeEach((to, from, next) => {
         next({ name: 'main' });
     } else {
         const timerStore = useTimerAndDateStore();
+        const timerTest = useTimerStore();
         timerStore.stopTimer(); // Останавливаем таймер
+        timerTest.stopTimer();
         next(); // Продолжаем переход
     }
   });
